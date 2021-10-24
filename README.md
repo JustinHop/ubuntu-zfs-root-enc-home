af33d376902a22bc57d1950682c43f8eea98c321
Justin Hoppensteadt <justinrocksmadscience@gmail.com>
2021-10-24 14:43:42 +0700

    First commit on convert.sh

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # ubuntu-zfs-root-enc-home
+Copyright 2021 Justin Hoppensteadt
 Additional per user encryption of home dir for Ubuntu systems with ZFS root
+
+Existing solutions either don't work on Ubuntu 21.10, or are manual processes
+that are not suitable for large, or production, environments.
+
+## Installation and usage
+1. Copy sbin/* to /sbin, chmod 0755 them, chown root:root them as well
+2. Manually merge the files in pam.d into your pam setup. This will be different
+   on various flavors/releases. I'll try to automate this in the future
+3. Do not be logged in as the user you want to migrate, best run logged in as
+   root on the console (no X)
+4. Read convert.sh and *understand* what it is going to do
+5. Make sure you are running a zfs root with bpool and rpool conventions
+6. Make sure that the user to be converted was either created using the normal
+   Ubuntu systems management tools, adduser, gnome-control-center, etc. The
+   user's home directory should be visible when running `zfs list`, and should
+   have the following convention for the volume name
+   "rpool/USERDATA/$USER_$RANDOM" where USER is the user, and RANDOM is a string
+   that can be described by this pcre `/[\w\d]{6}/`
+7. Run `./convert.sh --help` and decide if you need to add any arguments,
+   referenced as $ARGUMENTS below
+8. Don't skip 4, 5, 6, or 7
+9. On the console, as root, run `./convert.sh $ARGUMENTS $USER`, $USER is the user to be
+   converted.
+
+# Special thanks
+This project automates code from, adds to, was inspired by, etc
+- https://talldanestale.dk/2020/04/06/zfs-and-homedir-encryption/
+- https://www.1stbyte.com/2021/04/12/encrypt-your-zfs-home-directory-in-ubuntu-20-04/
+- https://medium.com/@andaag/how-i-moved-a-ext4-ubuntu-install-to-encrypted-zfs-62af1170d46c
+- https://github.com/rlaager/zfscrypt
+- https://github.com/openzfs/zfs
+- https://ubuntu.com
+
+
+# Special thanks to me
+- btc: bc1qqxrjrzw9mwa49tv6wucrz22xr5ga4kg8ptl9x5
---
+++ convert.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Set some default values:
+HELP=
+KEYFILE=
+RMKEYFILE=
+NOOP=
+VERBOSE=
+ZFSUSER=
+
+usage()
+{
+  echo "Usage: $0  [ -h | --help ]
+  [ -k | --keyfile KEYFILE ]
+  [ -n | --noop ]
+  [ -r | --remove ]
+  [ -v | --verbose ] ZFSUSER
+
+  KEYFILE = Plain text passphrase in a file, should be the same as passwd for user
+  noop    = No actions taken
+  remove  = Remove inital unencrypted zfs volume for user, be careful
+  ZFSUSER = Username of existing user with zfs mounted home dir"
+
+  exit 2
+}
+
+PARSED_ARGUMENTS=$(getopt -a -n $0 -o hk:nrv --long help,keyfile:,noop,remove,verbose -- "$@")
+VALID_ARGUMENTS=$?
+if [ "$VALID_ARGUMENTS" != "0" ]; then
+  usage
+fi
+
+eval set -- "$PARSED_ARGUMENTS"
+while :
+do
+  case "$1" in
+    -h | --help)    HELP=1          ; shift   ;;
+    -k | --keyfile) KEYFILE="$2"    ; shift 2 ;;
+    -n | --noop)    NOOP=echo       ; shift   ;;
+    -r | --remove)  RMKEYFILE=1     ; shift   ;;
+    -v | --verbose) VERBOSE=1       ; shift   ;;
+    # -- means the end of the arguments; drop this, and break out of the while loop
+    --) shift; break ;;
+    # If invalid options were passed, then getopt should have reported an error,
+    # which we checked as VALID_ARGUMENTS when getopt was called...
+    *) echo "Unexpected option: $1 - this should not happen."
+    usage ;;
+esac
+done
+
+if [ $HELP ]; then
+  usage
+  exit 2
+fi
+
+# BEGIN IN DEVELOPMENT
+NOOP=echo
+VERBOSE=1
+set -x
+# END IN DEVELOPMENT
+
+if [ $VERBOSE ]; then
+  echo "PARSED_ARGUMENTS is $PARSED_ARGUMENTS"
+  echo "HELP    : $HELP"
+  echo "KEYFILE : $KEYFILE"
+  echo "NOOP    : $NOOP"
+  echo "VERBOSE : $VERBOSE"
+  echo "Parameters remaining are: $@"
+fi
+
+PCMD=$NOOP
+if [ $(whoami) != "root" ]; then
+  # set stuff for non root users and make sure sudo works
+  PCMD="$PRECMD sudo"
+  sudo true
+fi
+
+for ZFSUSER in $@; do
+  [ $VERBOSE ] && echo "ZFSUSER : $ZFSUSER"
+  ZUSER=$ZFSUSER
+  ZUID=$(getent passwd $ZFSUSER | cut -d: -f3)
+  ZGID=$(getent passwd $ZFSUSER | cut -d: -f4)
+  ZHOME=$(getent passwd $ZFSUSER | cut -d: -f6)
+  if [ $VERBOSE ]; then
+    echo "ZUSER  : $ZUSER"
+    echo "ZUID   : $ZUID"
+    echo "ZGID   : $ZGID"
+    echo "ZHOME  : $ZHOME"
+  fi
+  if ps -u $ZUID ; then
+    echo "There are processes being run by $ZFSUSER UID=$ZUID GID=$ZGID."
+    echo "Kill those processes and try again"
+    exit 1
+  else
+    if [ $VERBOSE ]; then
+      echo "Found no processes with UID=$ZUID"
+    fi
+  fi
+  zfs get mountpoint -s local -H -o name,value | while read ZVOLNAME ZMOUNTPOINT; do
+    [[ $ZMOUNTPOINT = $ZHOME ]] || continue
+
+    ZVOLNAMENE=${ZVOLNAME}_noenc
+    ZSNAPNAME=${ZVOLNAME}@premigrate2enc
+    ZBOOTFS=$(zfs get com.ubuntu.zsys:bootfs-datasets rpool/USERDATA/yield_iul2j0 -s local -H -o value)
+    if [ $VERBOSE ]; then
+      echo "ZVOLNAME    : $ZVOLNAME"
+      echo "ZVOLNAMENE  : $ZVOLNAMENE"
+      echo "ZSNAPNAME   : $ZSNAPNAME"
+      echo "ZMOUNTPOINT : $ZMOUNTPOINT"
+      echo "ZBOOTFS     : $ZBOOTFS"
+    fi
+    if ! [ $KEYFILE ] || ! [ -f $KEYFILE ] ; then
+      read -s -p "Encryption key for $ZUSER, should be same as password:" ZPASSPHRASE
+      RMKEYFILE=1
+      KEYFILE=$(mktemp)
+      export KEYFILE
+      export ZPASSPHRASE
+      bash -c 'cat > $KEYFILE <<< $ZPASSPHRASE'
+    fi
+    $PCMD zfs rename $ZVOLNAME $ZVOLNAMENE
+    $PCMD zfs set mountpoint=none $ZVOLNAMENE
+    $PCMD zfs snapshot $ZSNAPNAME
+    ZCMD="$PCMD zfs send -v -c $ZVOLNAMENE | $PCMD zfs recv -v -o encryption=aes-256-gcm -o keyformat=passphrase -o keylocation=file://$KEYFILE -o mountpoint=$ZHOME -o com.ubuntu.zsys:bootfs-dataset=$ZBOOTFS $ZVOLNAME"
+    if [ $NOOP ]; then
+      echo "$ZCMD"
+    else
+      eval $ZCMD
+    fi
+    $PCMD zfs set keylocation=prompt $ZVOLNAME
+    $PCMD zfs set canmount=noauto $ZVOLNAME
+    $PCMD zfs set io.github.justinhop:user=$ZUSER $ZVOLNAME
+    if [ $RMKEYFILE ]; then
+      $PCMD shred $KEYFILE
+      $PCMD rm $KEYFILE
+    fi
+  done
+done
